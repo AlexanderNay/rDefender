9830906abaaa9d9d3a512d7f76bde633e88bea07
AlexanderN <>
2019-08-27 22:21:37 +0300

    Main screen was correct to fit all screen size types

--- rDefender/MainViewController.swift
+++ rDefender/MainViewController.swift
@@ -22,13 +22,44 @@ class MainViewController: UIViewController {
     
     var guestType: GuestType!
     
+    
+    //Constraints
+        //Rasskazovo Label
+    @IBOutlet weak var rasskazovoLabelHighConstraint: NSLayoutConstraint!
+    @IBOutlet weak var rasskazovoLabelWidthConstraint: NSLayoutConstraint!
+    
+        //Sign a Pass Label
+    @IBOutlet weak var signPassLabelTopConstraint: NSLayoutConstraint!
+    
+        //Guest Buttons Stack
+    @IBOutlet weak var guestButtonsStackTopConstraint: NSLayoutConstraint!
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         setupUI()
         setNavigationBar()
+        setConstraintsForSmallScreens()
         
     }
+    
+    func setConstraintsForSmallScreens() {
+        
+        //TODO: I don't like it
+        switch UIDevice().type {
+        case .iPhoneSE, .iPhone5, .iPhone5S, .iPhone6, .iPhone7, .iPhone8, .iPhone6S:
+            rasskazovoLabelHighConstraint.constant = 150/1.5
+            rasskazovoLabelWidthConstraint.constant = 200/1.5
+            signPassLabelTopConstraint.constant = 16
+            guestButtonsStackTopConstraint.constant = 16
+        default: break
+        }
+    }
+        
     
     
     func setNavigationBar() {
@@ -58,10 +89,21 @@ class MainViewController: UIViewController {
         
         listOfGuestButton.layer.cornerRadius = 8
         listOfGuestButton.layer.masksToBounds = true
+        
+        //Set font
+        guestButton.titleLabel?.minimumScaleFactor = 0.5
+        guestButton.titleLabel?.adjustsFontSizeToFitWidth = true
+        carButton.titleLabel?.minimumScaleFactor = 0.5
+        carButton.titleLabel?.adjustsFontSizeToFitWidth = true
+        truckButton.titleLabel?.minimumScaleFactor = 0.5
+        truckButton.titleLabel?.adjustsFontSizeToFitWidth = true
+        listOfGuestButton.titleLabel?.minimumScaleFactor = 0.5
+        listOfGuestButton.titleLabel?.adjustsFontSizeToFitWidth = true
     }
     
     
     @IBAction func guestButtonPressed(_ sender: UIButton) {
+        //TODO: I don't like it
         if sender.tag == 1 {
             guestType = .person
         }
